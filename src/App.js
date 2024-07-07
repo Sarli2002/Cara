3b9eda47d571fcdf897b5c3e58f77faa985b0876
Sarli <sarlisengar@gmail.com>
2024-07-07 15:04:23 +0530

    changes in app

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ function App() {
           <Route path="/product/:productId" element={<Product />} />
           <Route path="/cart" element={<Cart />} />
         </Routes>
-        <Footer />
+       
       </BrowserRouter>
     </div>
   );
